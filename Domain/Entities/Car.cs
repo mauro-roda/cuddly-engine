6d350e32df851c7946f5c7b209397fda0040e129
Mauro <mauro.roda@hotmail.com>
2024-04-30 17:48:10 +0100

    1 feature: Create Car

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MalfunctionChance { get; set; }
+        public int MalfunctionsOcurred { get; set; }
+        public int DistanceCoveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+    }
+}
