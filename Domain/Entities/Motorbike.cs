d7729498be78450e094fe75336a5d223e80b56ae
Mauro <mauro.roda@hotmail.com>
2024-04-30 18:28:15 +0100

    2.1 feature: Created motorbike entity and API

---
+++ Domain/Entities/Motorbike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Motorbike
+    {
+        public int Id { get; set; }
+        public string? TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MalfunctionChance { get; set; }
+        public int MalfunctionsOcurred { get; set; }
+        public int DistanceCoveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+    }
+}
---
+++ WebUI/Features/Motorbikes/MotorbikesController.cs
@@ -0,0 +1,85 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Features.Motorbikes
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotorbikesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Motorbike>> GetMotorbikes()
+        {
+            var motos = new List<Motorbike>();
+            var moto1 = new Motorbike()
+            {
+                TeamName = "Team A",
+                Speed = 100,
+                MalfunctionChance = 0.2
+            };
+
+            var moto2 = new Motorbike()
+            {
+                TeamName = "Team B",
+                Speed = 90,
+                MalfunctionChance = 0.15
+            };
+
+            motos.Add(moto1);
+            motos.Add(moto2);
+
+            return Ok(motos);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Motorbike> GetMotorbike(int id)
+        {
+            var moto1 = new Motorbike()
+            {
+                TeamName = "Team A",
+                Speed = 100,
+                MalfunctionChance = 0.2
+            };
+
+            return Ok(moto1);
+        }
+
+        [HttpPost]
+        public ActionResult<Motorbike> CreateMotorbike(Motorbike motorbike)
+        {
+            var newMoto = new Motorbike()
+            {
+                Id = motorbike.Id,
+                TeamName = motorbike.TeamName,
+                Speed = motorbike.Speed,
+                MalfunctionChance = motorbike.MalfunctionChance
+            };
+
+            return Ok(newMoto);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult<Motorbike> UpdateMotorbike(Motorbike motorbike)
+        {
+            var updateMoto = new Motorbike()
+            {
+                Id = motorbike.Id,
+                TeamName = motorbike.TeamName,
+                Speed = motorbike.Speed,
+                MalfunctionChance = motorbike.MalfunctionChance
+            };
+
+            return Ok(updateMoto);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult DeleteMotorbike(int id)
+        {
+            return Ok($"Motorbike with ID: {id} was succesfully deleted.");
+        }
+    }
+}
