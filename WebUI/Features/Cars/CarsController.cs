8b2dbaa4de5d13440deb3c5dab4cc77c42670fae
Mauro <mauro.roda@hotmail.com>
2024-04-30 17:48:40 +0100

    1 feature: Create Cars controller

---
+++ WebUI/Features/Cars/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Features.Cars
+{
+    [Route("api/cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Car>> GetCars()
+        {
+            var cars = new List<Car>();
+            var car1 = new Car()
+            {
+                TeamName = "Team A",
+                Speed = 100,
+                MalfunctionChance = 0.2
+            };
+
+            var car2 = new Car()
+            {
+                TeamName = "Team B",
+                Speed = 90,
+                MalfunctionChance = 0.15
+            };
+
+            cars.Add(car1);
+            cars.Add(car2);
+
+            return Ok(cars);
+        }
+    }
+}
