902033a2e2d70425e224076e54a7af23ced635c9
Mauro <mauro.roda@hotmail.com>
2024-05-01 13:12:20 +0100

    3 feature: Add application DB context

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
