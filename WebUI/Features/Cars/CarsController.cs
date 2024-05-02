24e9d3e1a468dd51d7a72517189cdbf47d24b1f8
Mauro <mauro.roda@hotmail.com>
2024-05-02 20:04:21 +0100

    4 refactor: Added Car Models

--- WebUI/Features/Cars/CarsController.cs
+++ WebUI/Features/Cars/CarsController.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Entities;
 using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using WebUI.Features.Cars.Models;
 
 namespace WebUI.Features.Cars
 {
@@ -39,8 +40,15 @@ namespace WebUI.Features.Cars
         }
 
         [HttpPost]
-        public ActionResult<Car> CreateCar(Car car)
+        public ActionResult<Car> CreateCar(CarCreateModel carModel)
         {
+            var car = new Car
+            {
+                TeamName = carModel.TeamName,
+                Speed = carModel.Speed,
+                MalfunctionChance = carModel.MalfunctionChance
+            };
+
             _context.Cars.Add(car);
             _context.SaveChanges();
 
@@ -49,18 +57,18 @@ namespace WebUI.Features.Cars
 
         [HttpPut]
         [Route("{id}")]
-        public ActionResult<Car> UpdateCar(Car car)
+        public ActionResult<Car> UpdateCar(CarUpdateModel carModel)
         {
-            var dbCar = _context.Cars.FirstOrDefault(c => c.Id == car.Id);
+            var dbCar = _context.Cars.FirstOrDefault(c => c.Id == carModel.Id);
 
             if (dbCar == null)
             {
-                return NotFound($"Car with ID: {car.Id} has not been found");
+                return NotFound($"Car with ID: {carModel.Id} has not been found");
             }
 
-            dbCar.TeamName = car.TeamName;
-            dbCar.Speed = car.Speed;
-            dbCar.MalfunctionChance = car.MalfunctionChance;
+            dbCar.TeamName = carModel.TeamName;
+            dbCar.Speed = carModel.Speed;
+            dbCar.MalfunctionChance = carModel.MalfunctionChance;
 
             _context.SaveChanges();
 
